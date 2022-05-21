1948e39e2632c1e5ecdc55cd5c2c0c9f2f8fc9e1
thiago lopes de mello <timabuntu@gmail.com>
2022-05-21 15:07:32 -0300

    container header added

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -2,7 +2,8 @@ import Image from 'next/image';
 import logo from '../../public/images/logo.svg';
 export const Header: React.FC = () => {
   return (
-    <div>
+    <div className='w-full h-20 border-b border-gray-700'>
+      <header className='flex items-center h-full max-w-6xl px-8 mx-auto'>
       <figure>
         <Image src={logo} alt='logo ignews' />
       </figure>
