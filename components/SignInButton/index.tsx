7c09eb12fd76e765c552491dbfdc9d018a029bb8
thiago lopes de mello <timabuntu@gmail.com>
2022-05-23 19:02:50 -0300

    button state login

--- components/SignInButton/index.tsx
+++ components/SignInButton/index.tsx
@@ -1,12 +1,27 @@
-import { GithubLogo } from 'phosphor-react';
+import { GithubLogo, X } from 'phosphor-react';
 export const SignInButton = () => {
-  return (
+  const isUserLoggedIn = true;
+
+  return isUserLoggedIn ? (
     <button
       type='button'
       className='h-12 px-6 transition rounded-full bg-zinc-700 hover:brightness-75'
     >
       <div className='flex items-center justify-center gap-4 '>
-        <div className='p-1 bg-yellow-300 rounded-full'>
+        <div className='p-1 bg-green-400 rounded-full'>
+          <GithubLogo weight='fill' size={18} color='#3f3f46' />
+        </div>
+        <p className='font-semibold'>Thiago Mello</p>
+        <X color='#737380' />
+      </div>
+    </button>
+  ) : (
+    <button
+      type='button'
+      className='h-12 px-6 transition rounded-full bg-zinc-700 hover:brightness-75'
+    >
+      <div className='flex items-center justify-center gap-4 '>
+        <div className='p-1 bg-yellow-400 rounded-full'>
           <GithubLogo weight='fill' size={20} color='rgb(63 63 70)' />
         </div>
         <p className='font-semibold'>Sign in with Github</p>
