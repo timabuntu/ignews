21b355aa6e999fc5d2909dcf633f767aff98d187
thiago lopes de mello <timabuntu@gmail.com>
2022-05-23 20:48:53 -0300

    width changed for container

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -4,12 +4,12 @@ import { SignInButton } from '../SignInButton';
 export const Header = () => {
   return (
     <div className='w-full h-20 border-b border-zinc-700'>
-      <header className='flex items-center h-full max-w-6xl mx-auto'>
+      <header className='container flex items-center h-full mx-auto'>
         <figure className='grid'>
           <Image src={logo} alt='logo ignews' />
         </figure>
 
-        <nav className='flex w-auto gap-1 ml-20 '>
+        <nav className='flex w-auto gap-1 ml-20'>
           <a className="flex items-center justify-center relative h-20 px-2 cursor-pointer text-zinc-300 hover:text-zinc-50 active:after:content-[''] active:after:h-1 active:after: active:after:rounded-tl-sm active:after:rounded-tr-sm active:after:w-full active:after:absolute active:after:bottom-[1px] active:after:left-0 active:after:bg-yellow-500 active:text-zinc-50 active:font-bold">
             Home
           </a>
