2c31a1821ac00b90916e9ac8ec1da1cda02ab863
thiago lopes de mello <timabuntu@gmail.com>
2022-05-24 10:47:29 -0300

    SubscribeButton typing defined

--- components/SubscribeButton/index.tsx
+++ components/SubscribeButton/index.tsx
@@ -1,4 +1,8 @@
-export const SubscribeButton = () => {
+interface SubscribeButtonProps {
+  priceId: number;
+}
+
+export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
   return (
     <button className='w-64 h-16 mt-10 font-bold transition bg-yellow-500 rounded-full hover:brightness-75 text-zinc-900'>
       Subscribe now
