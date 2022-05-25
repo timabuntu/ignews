0dd06b8d747e637e98c8848bad3284817f60588e
thiago lopes de mello <timabuntu@gmail.com>
2022-05-25 12:50:07 -0300

    exported nextauth default

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,8 +1,11 @@
+import NextAuth from 'next-auth';
 import GitHubProvider from 'next-auth/providers/github';
 
-providers: [
-  GitHubProvider({
-    clientId: process.env.GITHUB_CLIENT_ID,
-    clientSecret: process.env.GITHUB_CLIENT_SECRET,
-  }),
-];
+export default NextAuth({
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+});
