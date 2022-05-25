8eaec0a1546be26e61562c48fcd1375211580d3c
thiago lopes de mello <timabuntu@gmail.com>
2022-05-25 12:53:29 -0300

    feat: created login with github api authentication

--- components/SignInButton/index.tsx
+++ components/SignInButton/index.tsx
@@ -1,17 +1,21 @@
 import { GithubLogo, X } from 'phosphor-react';
-export const SignInButton = () => {
-  const isUserLoggedIn = true;
+import { signIn, signOut, useSession } from 'next-auth/react';
 
-  return isUserLoggedIn ? (
+export const SignInButton = () => {
+  const { data: session } = useSession();
+  //  console.log(session);
+
+  return session ? (
     <button
       type='button'
       className='h-12 px-6 transition rounded-full bg-zinc-700 hover:brightness-75'
+      onClick={() => signOut()}
     >
       <div className='flex items-center justify-center gap-4'>
         <div className='p-1 bg-green-400 rounded-full'>
           <GithubLogo weight='fill' size={18} color='#3f3f46' />
         </div>
-        <p className='font-semibold'>Thiago Mello</p>
+        <p className='font-semibold'>{session.user?.name}</p>
         <X color='#737380' />
       </div>
     </button>
@@ -19,6 +23,7 @@ export const SignInButton = () => {
     <button
       type='button'
       className='h-12 px-6 transition rounded-full bg-zinc-700 hover:brightness-75'
+      onClick={() => signIn('github')}
     >
       <div className='flex items-center justify-center gap-4'>
         <div className='p-1 bg-yellow-500 rounded-full'>
