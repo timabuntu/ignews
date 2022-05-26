7d690e06d6bd9706fb065224e4451da08a6d5bca
thiago lopes de mello <timabuntu@gmail.com>
2022-05-26 11:06:38 -0300

    created function handleSubscribe

--- components/SubscribeButton/index.tsx
+++ components/SubscribeButton/index.tsx
@@ -1,11 +1,24 @@
+import { useSession, signIn } from 'next-auth/react';
+
 interface SubscribeButtonProps {
   priceId: string;
 }
 
-export const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+const SubscribeButton = ({ priceId }: SubscribeButtonProps) => {
+  const { data: session } = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+  }
+
   return (
     <button className='w-64 h-16 mt-10 font-bold transition bg-yellow-500 rounded-full hover:brightness-75 text-zinc-900'>
       Subscribe now
     </button>
   );
 };
+
+export default SubscribeButton;
