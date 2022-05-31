1ca0f2367b1c3b47ed77507d3cb0f6a91ec9a99e
thiago lopes de mello <timabuntu@gmail.com>
2022-05-31 10:03:23 -0300

    created request buffer

--- pages/api/webhooks.ts
+++ pages/api/webhooks.ts
@@ -1,6 +1,24 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+import { Readable } from 'stream';
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
-  console.log('evento recebido');
+async function buffer(readable: Readable) {
+  const chunks = [];
+
+  for await (const chunck of readable) {
+    chuncks.push(typeof chunck === 'string' ? Buffer.from(chunck) : chunck);
+  }
+  return Buffer.concat(chunks);
+}
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+
+// eslint-disable-next-line import/no-anonymous-default-export
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  const buf = await buffer(req);
+
   res.status(200).json({ ok: true });
 };
